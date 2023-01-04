d8114e1bd6814c418bc5391354ba049d777cdc99
Ivan Bushchik <ivabus@ivabus.dev>
2023-01-04 12:54:32 +0300

    0*.sh: Add scripts to build packages for repo.
    00_build_repo.sh for building repository.
    01_build_kernel.sh for building kernel (using configs from AsahiLinux/PKGBUILDs) called from 00_build_repo.sh
    
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

---
+++ scripts/01_build_kernel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script clones & builds all RPMs that are used in repository.
+
+_rcver=6.1
+_asahirel=3
+pkgrel=1
+_commit_id=asahi-${_rcver}${_rcrel+-rc}${_rcrel}-${_asahirel}
+makethreads=48
+CC=distcc
+CXX=distcc
+#makethreads=`distcc -j`
+mkdir build
+cd build
+
+echo "Downloading linux-asahi sources"
+curl -fSLO https://github.com/AsahiLinux/linux/archive/refs/tags/${_commit_id}.tar.gz
+echo "Unpacking linux-asahi"
+tar xpf ${_commit_id}.tar.gz
+curl -o config -fsSL https://raw.githubusercontent.com/AsahiLinux/PKGBUILDs/main/linux-asahi/config
+sed -i -e s/ARCH/opensuse/ config
+
+cd linux-${_commit_id}
+echo "Building main kernel"
+cp ../config .config
+make olddefconfig prepare
+diff -u ../config .config || :
+make -j$makethreads CC=$CC CXX=$CXX rpm-pkg
